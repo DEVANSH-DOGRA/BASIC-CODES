28dadadb4d375ebaade75ef1cf4b316109a68f3c
DEVANSH-DOGRA <devansh0422.be21@chitkara.edu.in>
2022-03-21 22:17:06 +0530

    IST COMMIT OF GREATEST

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    int g;
+    cout << "Enter the array elements: " << endl;
+    for(int i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    }
+    g=arr[1];
+    for(int i=0;i<=5;i++)
+    {
+        if (arr[i]>g)
+            g=arr[i];
+    }
+    cout<<"The greatest element is :"<<g<<endl;
+
+    return 0;
+}
