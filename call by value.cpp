ac824cf5206637e3cf4039a8c26631d5daea3a86
DEVANSH-DOGRA <devansh0422.be21@chitkara.edu.in>
2022-04-10 19:13:14 +0530

    added call by value

---
+++ call by value.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void swap(int x,int y)
+{
+int t;
+t=x;
+y=t;
+
+}
+
+int main()
+{
+int x=25,y=50;
+swap(x,y);
+cout<<"x="<<x<<endl;
+cout<<"y="<<y<<endl;
+getch();
+return 0;
+
+
+
+}
